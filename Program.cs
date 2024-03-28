2089df0e14ca1b6d0f38dd36f84a1fb2fec47408
Muhammad Hashfi Hadyan <116771617+hshinosa@users.noreply.github.com>
2024-03-28 08:58:21 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using TPMOD6;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Muhammad Hashfi Hadyan");
+        video.PrintVideoDetails();
+
+        for (int i = 0; i < 215; i++)
+        {
+            video.IncreasePlayCount(10000000);
+        }
+    }
+}
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMOD6
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string judul_video)
+        {
+            Random random = new Random();
+            
+            Debug.Assert(!string.IsNullOrEmpty(judul_video), "Judul tidak boleh kosong");
+            Debug.Assert(judul_video.Length <= 100, "Panjang judul tidak boleh melebihi 100 karakter");
+            this.title = judul_video;
+            this.id =  random.Next(10000,99999);
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int playCount)
+        {
+            Debug.Assert(playCount <= 10000000,"maksimum penambahan play count adalah 10.000.000 untuk setiap panggilan");
+            try
+            {
+                int tes = checked(this.playCount + playCount);
+                this.playCount = tes;
+            }catch(Exception overFlow)
+            {
+                Console.WriteLine(overFlow.Message);
+            }
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video id : " + id);
+            Console.WriteLine("Video title : " + title);
+            Console.WriteLine("Video play count : " + playCount);
+        }
+    }
+}
